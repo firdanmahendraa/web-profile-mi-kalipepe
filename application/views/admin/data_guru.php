9aa7c146fe7b4a0e61de540732574754caf99c27
Firdan Mahendra Wardana <72062343+firdanmahendraa@users.noreply.github.com>
2022-05-28 05:44:58 +0700

    header post

--- application/views/admin/data_guru.php
+++ application/views/admin/data_guru.php
@@ -1,1 +1,17 @@
-carousel
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1 class="m-0">Data Guru</h1>
+          </div><!-- /.col -->
+          <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+              <li class="breadcrumb-item"><a href="<?php echo base_url('dashboard') ?>">Home</a></li>
+              <li class="breadcrumb-item active">Data Guru</li>
+            </ol>
+          </div><!-- /.col -->
+        </div><!-- /.row -->
+      </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content-header -->
